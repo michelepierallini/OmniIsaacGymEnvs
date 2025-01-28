7ab5f801572b4dd22f3aa26c1c110558ea55c331
michelepierallini <michele.pierallini@gmail.com>
2025-01-28 22:45:52 +0100

    Update .gitignore and add fishing rod tasks plus model-based [working]

---
+++ my_push.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+git add omniisaacgymenvs/tasks/fishing_rod_real_pos_2.py
+git add omniisaacgymenvs/tasks/fishing_rod_real_pos_2_cv.py
+git add omniisaacgymenvs/cfg/task/FishingRodPosDueCV.yaml
+git add omniisaacgymenvs/cfg/train/FishingRodPosDueCVPPO.yaml
+
+git add omniisaacgymenvs/pyenvs.sh
+git add omniisaacgymenvs/tasks/fishing_rod_real_pos_2_cv_model_based.py
+git add omniisaacgymenvs/cfg/task/FishingRodPosDueCVMB.yaml
+git add omniisaacgymenvs/cfg/train/FishingRodPosDueCVMBPPO.yaml
+git add omniisaacgymenvs/tasks/fishing_rod_lumped_param/
+git add omniisaacgymenvs/utils/task_util.py
+
+git add omniisaacgymenvs/tasks/fishing_rod_real_pos_2_cv_model_based_pos.py
+git add omniisaacgymenvs/cfg/train/FishingRodPosDueCVMBPosPPO.yaml
+git add omniisaacgymenvs/cfg/task/FishingRodPosDueCVMBPos.yaml
+
+git add my_push.sh 
+git add .gitignore
+
+# #######################################################
+# if [ -n "$1" ]; then
+#   COMMIT_MESSAGE="$1"
+# else
+#   COMMIT_MESSAGE="minor_changes"
+# fi
+# git commit -m "$COMMIT_MESSAGE"
+# #######################################################
+git commit -m "Update .gitignore and add fishing rod tasks plus model-based [working]"
+
+# Push to the fork
+git push my-fork main
+
+
